830b3b2f1c0920d7e2065a045f3329b7910513f7
TrueRyoB <121699111+TrueRyoB@users.noreply.github.com>
2025-04-11 15:45:47 -0500

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,6 +1,26 @@
+const functions = ["sin", "cos", "tan", "asin", "acos", "atan", "log", "ln"];
+
 const map = {
-    "" : 0,
+    "+" : 1,
+    "-": 1,
+    "*" : 2,
+    "/" : 2,
+    "%" : 2,
+    "**" : 3,
 };
+
+const isOperatorSymbol = (token) => ["+", "-", "*", "/", "**", "%"].includes(token);
+
+const ParseStatus = Object.freeze({
+    SUCCESS: "success",
+    INVALID_EXPRESSION: "invalid_expression",
+    UNMATCHED_BRACKET: "unmatched_bracket",
+    UNREGISTERED_TOKEN: "unregistered_token",
+    CONSECUTIVE_CONSTANT: "consecutive_constant",
+    UNPAIRED_OPERANT: "unpaired_operant",
+    INVALID_PARENTHESIS: "invalid_parenthesis",
+    FAILURE: "failure",
+});
 
 export function parse(f) {
     let tokens = tokenize(f);
@@ -12,26 +32,115 @@ export function parse(f) {
     }
     if (!ok) return { success: ParseStatus.INVALID_EXPRESSION, result: 0 };
     
+    if (!validateParenthesis(tokens)) return { success: ParseStatus.INVALID_PARENTHESIS, result: 0 };
     
+    let post;
+    [ok, post] = getPostfixNotation(tokens);
+    if (ok !== ParseStatus.SUCCESS) return { success: ok, result: 0 };
     
-    return { success: ParseStatus.SUCCESS, result: 1 };
+    let res;
+    [res, ok] = evaluatePostfix(post);
+    
+    return { success: ok, result: ok === ParseStatus.SUCCESS ? res : 0 };
 }
 
-const ParseStatus = Object.freeze({
-    SUCCESS: "success", 
-    INVALID_EXPRESSION: "invalid_expression",
-    FAILURE: "failure",
-});
+function validateParenthesis(tokens) {
+    let stack = [];
+    const openers = { "(": ")", "[": "]" };
+    const closers = { ")": "(", "]": "[" };
 
+    for (let t of tokens) {
+        if (t in openers)
+            stack.push(t);
+        else if (t in closers) {
+            if (stack.length === 0 || stack[stack.length - 1] !== closers[t]) {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+    return stack.length === 0;
+}
 
-const functions = ["sin", "cos", "tan", "asin", "acos", "atan", "log", "ln"];
+function evaluatePostfix (post) {
+    let numStack = [];
+    for(let i = 0; i < post.length; i++) {
+        let t = post[i];
+        if (isNumber(t)) {
+            t = parseFloat(t);
+            numStack.push(t);
+        } else if (isConstant(t)) {
+            if (t === "π") t = Math.PI;
+            else if (t === "e") t = Math.E;
+            else return { success: ParseStatus.UNREGISTERED_TOKEN, result: 0 };
+            numStack.push(t);
+        } else {
 
-//TODO: not implemented
+            if (numStack.length < 2) return { success: ParseStatus.UNPAIRED_OPERANT, result: 0 };
+
+            let v2 = numStack.pop();
+            let v1 = numStack.pop();
+
+            switch (post[i]) {
+                case "+":
+                    numStack.push(v1 + v2);
+                    break;
+                case "-":
+                    numStack.push(v1 - v2);
+                    break;
+                case "*":
+                    numStack.push(v1 * v2);
+                    break;
+                case "/":
+                    numStack.push(v1 / v2);
+                    break;
+                case "%":
+                    numStack.push((v1 % v2 + v2) % v2);
+                    break;
+                case "**": //TODO: associativityを考慮して変更する
+                    numStack.push(v1 ** v2);
+                    break;
+                case "ln":
+                    numStack.push(v1);
+                    numStack.push(Math.log(v2));
+                    break;
+                case "sin":
+                    numStack.push(v1);
+                    numStack.push(Math.sin(v2));
+                    break;
+                case "cos":
+                    numStack.push(v1);
+                    numStack.push(Math.cos(v2));
+                    break;
+                case "tan":
+                    numStack.push(v1);
+                    numStack.push(Math.tan(v2));
+                    break;
+                case "asin":
+                    numStack.push(v1);
+                    numStack.push(Math.asin(v2));
+                    break;
+                case "acos":
+                    numStack.push(v1);
+                    numStack.push(Math.acos(v2));
+                    break;
+                case "atan":
+                    numStack.push(v1);
+                    numStack.push(Math.atan(v2));
+                    break;
+                default:
+                    alert(`Error: unrecognized token ${post[i]}!"`);
+                    return { success: ParseStatus.UNREGISTERED_TOKEN, result: 0 };
+            }
+        }
+    }
+    if(numStack.length > 1) return { success: ParseStatus.UNPAIRED_OPERANT, result: 0 };
+    return { success: ParseStatus.SUCCESS, result: numStack[0] };
+}
+
 function getPostfixNotation(tokens) {
     let opStack = [];
     let outStack = [];
-
-    // TODO: 演算記号と括弧と関数の優先度を記した辞書を作る
     
     for (let i = 0; i < tokens.length; i++) {
         let t = tokens[i];
@@ -39,40 +148,71 @@ function getPostfixNotation(tokens) {
         if (isNumber(t)) {
             outStack.push(t);
         }
+        // Assume that the rest is all operational symbols
         else {
-            // assume that the rest is all operational symbols
             
-            // 0. if it's a closing bracket, pop all to outStack until meeting a closing bracket
+            // 0. push an open bracket to opStack no matter what
+            if (["(", "["].includes[t]) {
+                opStack.push(t);
+                continue;
+            }
             
-            // 1. compare the current token and the peek of opStack
-            // 2. if it's CeqP or !CgP, insert it
-            // 3. put it to opStack if else
+            // 1. if it's a closing bracket, pop all to outStack until meeting a closing bracket
+            if ([")", "]"].includes[t]) {
+                let opening = t === ")" ? "(" : "[";
+                while (opStack.length > 0 && opStack[opStack.length - 1] !== opening) outStack.push(opStack.pop());
+                if (opStack.length === 0) {
+                    alert(`Error: unmatched closing parenthesis!"`);
+                    return { success: ParseStatus.UNMATCHED_BRACKET, result: null };
+                }
+                opStack.pop();
+                // push the subsequent function operator as well if there exists one
+                if (opStack.length > 0 && functions.includes(opStack[opStack.length - 1])) outStack.push(opStack.pop());
+                continue;
+            }
+            
+            // 2. compare the current token and the peek of opStack
+            if (! (t in map)) {
+                alert(`Error: token "${t} is not registered to the precedence map!"`);
+                return { success: ParseStatus.UNREGISTERED_TOKEN, result: null };
+            }
+            
+            // 3. Push to the appropriate stack //TODO: 不安
+            let shouldPushToOutput = opStack.length > 0 && map[t] <= map[opStack[opStack.length - 1]];
+            
+            if (shouldPushToOutput) outStack.push(t);
+            else opStack.push(t);
         }
     }
     
-    // 4. bump all elements in opStack to outStack
+    // 3. bump all elements in opStack to outStack
+    while (opStack.length > 0) outStack.push(opStack.pop());
     
-    return { success: true, result: outStack };
+    return { success: ParseStatus.SUCCESS, result: outStack };
 }
 
-
-//TODO: many errors
-function parseExpression(tokens, i = 0) {
+function parseExpression(tokens, i) {
     if (i >= tokens.length) return [false, i];
 
-    // General independent items
-    const t = tokens[i];
-    if (isNumber(t) || isConstant(t) || isOperatorSymbol(t)) { //TODO: 数字群の間にoperatorが挟まらない場合は違法とする　operatorの連続も違法とする
-        return [true, i + 1];
+    // Numbers
+    if (isNumber(tokens[i]) || isConstant(tokens[i])) {
+        const pre = tokens[i-1];
+        return [i === 0 || !(isNumber(pre) || isConstant(pre)), i + 1];
+    }
+    
+    // Operator symbols
+    if (isOperatorSymbol(tokens[i])) {
+        const pre = tokens[i-1];
+        return [i === 0 || !isOperatorSymbol(pre), i + 1];
     }
     
     // Pairs
-    if (isParenthesis(t) || isSquareBracket(t)){ //TODO: 同様に過去のデータ次第でtrueかどうかが変わる場合の実装方法を検討する
+    if (isParenthesis(tokens[i]) || isSquareBracket(tokens[i])){ //TODO: 同様に過去のデータ次第でtrueかどうかが変わる場合の実装方法を検討する
         return [true, i + 1];
     }
     
     // log(...)
-    if (t === "log") {
+    if (tokens[i] === "log") {
         i++;
         let ok;
         if (tokens[i] === "[") {
@@ -98,7 +238,7 @@ function parseExpression(tokens, i = 0) {
     }
 
     // trig's and ln
-    if (functions.includes(tokens[i])) { //TODO: sin^5(30)とかは有効にしないといけないから...expressionを(で終わるようにしないと
+    if (functions.includes(tokens[i])) {
         if (tokens[++i] !== "(") return [false, i];
         let ok;
         [ok, i] = parseExpression(tokens, ++i);
@@ -124,11 +264,15 @@ function tokenize(s) {
         if (isDigit(s[i])) {
             let j = i;
             let periodSeen = false;
+            let exponentIndex = -310;
 
-            while (j < n && (isDigit(s[j]) || (!periodSeen && s[j] === "."))) {
+            while (j < n && (isDigit(s[j]) || (!periodSeen && s[j] === ".") || (exponentIndex !== -310 && s[j] === "e") || (j === exponentIndex+1 && s[j] === "-"))) {
                 if (s[j] === ".") periodSeen = true;
+                if (s[j] === "e") exponentIndex = j;
                 j++;
             }
+            
+            if (j !== -310 && j < exponentIndex + 2) j = exponentIndex-1;
 
             tokens.push(s.slice(i, j));
             i = j;
@@ -139,8 +283,13 @@ function tokenize(s) {
         if (isOperatorSymbol(s[i])) {
             if (s[i] === "-" && (tokens.length === 0 || ["(", "["].includes(tokens[tokens.length - 1]))) tokens.push("0");
             
-            tokens.push(s[i]);
-            i++;
+            if (s[i] === "*" && i+1 < n && s[i+1] === "*") {
+                tokens.push("**");
+                i += 2;
+            } else {
+                tokens.push(s[i]);
+                i++;
+            }
             continue;
         }
 
@@ -187,27 +336,9 @@ function tokenize(s) {
 
 
 // helper methods
-function tryGetPrecedence(token) {
-    return { success: false, result : 0};
-} //TODO: fix this after creating a dictionary
-
-function isNumber(token) {
-    if(typeof token !== "string") return false;
-    
-    let periodSeen = false;
-    for (let i = 0; i < token.length; i++) {
-        if (isDigit(token[i]) || (!periodSeen && token[i] === ".")) {
-            if (token[i] === ".") periodSeen = true;
-            continue;
-        }
-        return false;
-    }
-    return true;
-}
+const isNumber = (token) => !isNaN(token);
 const isSquareBracket = (token) => ["[", "]"].includes(token);
 const isParenthesis = (token) => ["(", ")"].includes(token);
 const isSpace = (token) => /^\s+$/.test(token);
-const isOperatorSymbol = (token) => ["+", "-", "*", "/", "^", "%"].includes(token);
 const isDigit = (token) => /^\d+$/.test(token);
 const isConstant = (token) => ["π", "e"].includes(token);
-
