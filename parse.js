9168231ee732845e9aed284d0301cef37ee7a814
TrueRyoB <121699111+TrueRyoB@users.noreply.github.com>
2025-04-13 11:13:31 -0500

    Update parse.js

--- parse.js
+++ parse.js
@@ -57,7 +57,7 @@ const testcase = [
     //["-30e-2 + 10e", { solution: "-0.3+10e", isValid: true }], //TODO: 指数表記のtestcaseを増やしたい atan2に自動変換もしたい
 ];
 
-const currentVersion = "08:19";
+const currentVersion = "11:13";
 const functions = ["sin", "cos", "tan", "asin", "acos", "atan", "log", "ln"];
 
 const operators = {
